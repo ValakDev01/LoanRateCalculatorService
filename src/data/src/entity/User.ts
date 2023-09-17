d42f97db07078a98dfd46a08303588df33804da6
Dominik Walczak <valakdev01@dominiks-macbook-pro.home>
2023-09-17 13:38:02 +0200

    13. Improving file structure using prettier.

--- src/data/src/entity/User.ts
+++ src/data/src/entity/User.ts
@@ -1,21 +1,19 @@
-import { Entity, PrimaryGeneratedColumn, Column } from "typeorm"
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
 
 @Entity()
 export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @PrimaryGeneratedColumn()
-    id: number;
-  
-    @Column()
-    installmentAmount: number;
-  
-    @Column()
-    financingAmount: number;
-  
-    @Column()
-    referenceRate: number;
-  
-    @Column()
-    lastFetchDateResponse: Date;
+  @Column()
+  installmentAmount: number;
 
+  @Column()
+  financingAmount: number;
+
+  @Column()
+  referenceRate: number;
+
+  @Column()
+  lastFetchDateResponse: Date;
 }
--- src/data/src/index.ts
+++ src/data/src/index.ts
@@ -1,26 +1,29 @@
-import { AppDataSource } from "./data-source"
-import { User } from "./entity/User"
+import { AppDataSource } from "./data-source";
+import { User } from "./entity/User";
 import axios from "axios";
 
-AppDataSource.initialize().then(async () => {
-  try {
-    const response = await axios.get('http://localhost:3000/getReferenceRate');
+AppDataSource.initialize()
+  .then(async () => {
+    try {
+      const response = await axios.get(
+        "http://localhost:3000/getReferenceRate",
+      );
       const { savedRate, lastFetchDateResponse, nIAARR, nRFA } = response.data;
 
-        const user = new User();
-        user.installmentAmount = nIAARR;
-        user.financingAmount = nRFA;
-        user.referenceRate = savedRate;
-        user.lastFetchDateResponse = lastFetchDateResponse;
+      const user = new User();
+      user.installmentAmount = nIAARR;
+      user.financingAmount = nRFA;
+      user.referenceRate = savedRate;
+      user.lastFetchDateResponse = lastFetchDateResponse;
 
-        await AppDataSource.manager.save(user);
-        console.log("Saved a new rate with id: " + user.id);
+      await AppDataSource.manager.save(user);
+      console.log("Saved a new rate with id: " + user.id);
 
-        const users = await AppDataSource.manager.find(User);
-        console.log("Loaded rates: ", users);
-      } catch (error) {
-        console.log(error);
-      }
+      const users = await AppDataSource.manager.find(User);
+      console.log("Loaded rates: ", users);
+    } catch (error) {
+      console.log(error);
+    }
   })
   .catch((error) => {
     console.error(error);
